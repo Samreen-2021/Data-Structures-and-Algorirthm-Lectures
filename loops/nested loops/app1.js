aaba24e50959a25d27d3599f1ca4f45f7613d029
samreen-2021 <determinedlearner2019@gmail.com>
2021-06-27 12:55:56 +0500

    Implementation of Basic data structure string concept practiced

--- loops/nested loops/app1.js
+++ loops/nested loops/app1.js
@@ -1,4 +1,4 @@
-let i = 0;
+/*let i = 0;
 while (i <= 2) {
   console.log("- First level loop");
   i++;
@@ -7,4 +7,21 @@ while (i <= 2) {
     console.log("-- Second level loop");
     j++;
   }
+}
+let array = [[1,2], [3, 4], [5,6,7]];
+for(let i=0; i<array.length; i++){
+    for(let j=0; j<array[i].length; j++){
+        console.log(array[i][j]);
+    }
+}*/
+// now using while loop
+let arr = [[0,1,2], [3, 4], [5,6,7]];
+let i=0;
+while(i< arr.length){
+    let j=0;
+    while(j<arr[i].length){
+        console.log(arr[i][j]);
+        j++;
+    }
+    i++;     
 }
---
+++ strings/strings.js
@@ -0,0 +1,4 @@
+let name ="samreen";
+ for(let i=0; i<name.length; i++){
+     console.log(name[i] + " is  string character");
+ }
